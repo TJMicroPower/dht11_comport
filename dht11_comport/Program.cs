2422ffaf3c038635c11ccd4310b3952ddbde4aed
Batuhan Bayır <batuhan@batuhanbayir.com>
2015-04-12 16:08:32 +0300

    DHT11 COM Port

---
+++ dht11_comport/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace dht11_comport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////////////// KONSOL AYARLARI ///////////////////////////////////
+            Console.SetWindowSize(70, 30);
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            /////////////////////////////////////////////////////////////////
+            
+            
+            /////////////////////////////////////////////////////////////////
+            SerialPort myport = new SerialPort();
+            myport.BaudRate = 9600;
+            myport.PortName = "COM3";
+            myport.Open();
+            /////////////////////////////////////////////////////////////////
+
+
+            /////////////////////////////////////////////////////////////////
+            bool devametsinmi = true;
+            while (devametsinmi && true)
+            {
+                string data_rx = myport.ReadLine();
+                Console.WriteLine(data_rx);
+            }
+            /////////////////////////////////////////////////////////////////
+        }
+    }
+}
